7f8f1c720bcfbf2a66616d4c990b3637fce28223
Lucas Tierney <lucast1533@gmail.com>
2017-04-10 22:24:54 -0500

    load atoms from the correct directory

--- watir_snake/atoms.py
+++ watir_snake/atoms.py
@@ -1,11 +1,13 @@
 from os import path
 
+
 def load_atoms(*args, **kwargs):
     cls = type(*args, **kwargs)
     cls.load('fireEvent')
     cls.load('getOuterHtml')
     cls.load('getInnerHtml')
     return cls
+
 
 class Atoms(object):
     __metaclass__ = load_atoms
@@ -14,7 +16,7 @@ class Atoms(object):
 
     @classmethod
     def load(cls, function_name):
-        filepath = path.abspath(path.join(path.dirname(__file__), '..', 'atoms',
+        filepath = path.abspath(path.join(path.dirname(__file__), 'atoms',
                                           '{}.js'.format(function_name)))
         with open(filepath, 'r') as myfile:
             cls.ATOMS[function_name] = myfile.read()
@@ -22,5 +24,6 @@ class Atoms(object):
     # private
 
     def _execute_atom(self, function_name, *args):
-      script = 'return ({}).apply(null, arguments)'.format(self.__class__.ATOMS.get(function_name))
-      return self.driver.execute_script(script, *args)
+        script = 'return ({}).apply(null, arguments)'.format(
+            self.__class__.ATOMS.get(function_name))
+        return self.driver.execute_script(script, *args)
