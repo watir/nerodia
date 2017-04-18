71c92019441bd7604572a79999aff2f1cc890b72
Lucas Tierney <lucast1533@gmail.com>
2017-04-17 20:59:33 -0500

    finish cookies

--- watir_snake/cookies.py
+++ watir_snake/cookies.py
@@ -1,4 +1,88 @@
-class Cookies(object):
+import time
+from json import dump, load
 
+
+class Cookies(object):
     def __init__(self, driver):
-        pass
+        self.driver = driver
+
+    @property
+    def to_list(self):
+        """
+        Returns array of cookies
+        :rtype: list[dict]
+
+        :Example:
+
+        browser.cookies.to_list
+        #=> [{name: 'my_session', value: 'BAh7B0kiD3Nlc3Npb25faWQGOgZFRkk', domain: 'mysite.com'}]
+        """
+        cookies = self.driver.get_cookies()
+        for cookie in cookies:
+            expire = cookie.get('expires')
+            cookie['expires'] = self._to_time(expire) if expire else None
+        return cookies
+
+    def __getitem__(self, name):
+        """
+        Returns a cookie by name
+        :param name: name of cookie
+        :rtype: dict
+
+        :Example:
+
+        browser.cookies['my_session']
+        #=> {name: 'my_session', value: 'BAh7B0kiD3Nlc3Npb25faWQGOgZFRkk', domain: 'mysite.com'}
+        """
+        return next((c for c in self.to_list if c.get('name') == name), None)
+
+    def add(self, name, value, **kwargs):
+        """
+        Adds new cookie
+
+        :Example:
+
+        browser.cookies.add('my_session', 'BAh7B0kiD3Nlc3Npb25faWQGOgZFRkk', domain='mysite.com')
+        """
+        cookie = {'name': name, 'value': value, 'secure': kwargs.get('secure'),
+                  'path': kwargs.get('path'), 'expires': kwargs.get('expires'),
+                  'domain': kwargs.get('domain')}
+        self.driver.add_cookie(cookie)
+
+    def delete(self, name):
+        """ Deletes cookie by given name """
+        self.driver.delete_cookie(name)
+
+    def clear(self):
+        """ Deletes all cookies """
+        self.driver.delete_all_cookies()
+
+    def save(self, file='.cookies'):
+        """
+        Save cookies to file
+        :param file: file path
+
+        :Example:
+
+        browser.cookies.save('.cookies')
+        """
+        with open(file, 'w') as f:
+            f.write(dump(self.to_list, f, indent=4))
+
+    def load(self, file='.cookies'):
+        """
+        Loads cookies from file
+        :param file: file path
+
+        :Example:
+
+        browser.cookies.load('.cookies')
+        """
+        with open(file, 'r') as f:
+            for cookie in load(f):
+                self.add(cookie.pop('name'), c.pop('value'), **c)
+
+    # private
+
+    def _to_time(self, dt):
+        return time.strftime('%Y-%m-%d %H:%M:%S %z', time.localtime(time.mktime(dt.timetuple())))
