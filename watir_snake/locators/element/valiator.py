5713d560eb661ffb10c7e8dba51fa35f9bb1ab22
Lucas Tierney <lucast1533@gmail.com>
2017-05-05 12:59:36 -0500

    add element locator validator

---
+++ watir_snake/locators/element/valiator.py
@@ -0,0 +1,17 @@
+
+class Validator(object):
+
+    @staticmethod
+    def validate(element, selector):
+        selector_tag_name = selector.get('tag_name')
+        element_tag_name = element.tag_name.lower()
+
+        if selector_tag_name and selector_tag_name != element_tag_name:
+            return None
+
+        if element_tag_name == 'input':
+            # TODO - Verify this is desired behavior based on https://bugzilla.mozilla.org/show_bug.cgi?id=1290963
+            if selector.get('type') and selector.get('type') != element.attribute('type').lower():
+                return None
+
+        return element
