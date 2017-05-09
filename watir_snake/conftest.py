eb536b34172d95ebaaf1ee3ab713f9df651bf32c
Lucas Tierney <lucast1533@gmail.com>
2017-05-08 23:04:48 -0500

    add requirements, conftest, webserver

---
+++ watir_snake/conftest.py
@@ -0,0 +1,76 @@
+import pytest
+from _pytest.skipping import MarkEvaluator
+from selenium.webdriver import DesiredCapabilities
+from watir_snake.browser import Browser
+
+from .support.webserver import WebServer
+
+browsers = (
+    'chrome',
+    'edge',
+    'firefox',
+    'ie',
+    'phantomjs'
+    # 'remote',  TODO: just local for now
+    'safari'
+)
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser',
+        action='store',
+        choices=browsers,
+        metavar='BROWSER',
+        help='browser to run tests against ({})'.format(', '.join(browsers)))
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    kwargs = {}
+
+    try:
+        browser_name = request.config.getoption('--browser')
+    except AttributeError:
+        raise Exception('This test requires a --browser to be specified.')
+
+    # conditionally mark tests as expected to fail based on driver
+    request.node._evalxfail = request.node._evalxfail or MarkEvaluator(
+        request.node, 'xfail_{}'.format(browser_name.lower()))
+
+    # skip driver instantiation if xfail(run=False)
+    if not request.config.getoption('runxfail'):
+        if request.node._evalxfail.istrue():
+            if request.node._evalxfail.get('run') is False:
+                yield
+                return
+
+    # if driver_class == 'remote':
+    #     capabilities = DesiredCapabilities.CHROME.copy()
+    #     kwargs.update({'desired_capabilities': capabilities})
+    browser = Browser(browser_name, executable_path='/Users/lucast/Documents/HC-Mercurial/hc.qa.webdriver/hc/qa/webdriver/binaries/darwin/chromedriver')
+    yield browser
+    try:
+        browser.quit()
+    except:
+        pass
+
+
+@pytest.fixture
+def page(browser, webserver):
+    class Page(object):
+        def url(self, name):
+            return webserver.path_for(name)
+
+        def load(self, name):
+            browser.goto(self.url(name))
+
+    return Page()
+
+
+@pytest.fixture(autouse=True, scope='session')
+def webserver():
+    webserver = WebServer()
+    webserver.start()
+    yield webserver
+    webserver.stop()
---
+++ watir_snake/support/webserver.py
@@ -0,0 +1,68 @@
+
+import logging
+from os import path, chdir
+import socket
+import threading
+from io import open
+try:
+    from urllib import request as urllib_request
+except ImportError:
+    import urllib as urllib_request
+try:
+    from http.server import BaseHTTPRequestHandler, HTTPServer, SimpleHTTPRequestHandler
+except ImportError:
+    from BaseHTTPServer import HTTPServer
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+HTML_ROOT = path.abspath(path.join(path.dirname(__file__), '..', '..', 'watir', 'spec',
+                                   'watirspec', 'html'))
+if not path.isdir(HTML_ROOT):
+    msg = 'Cannot find HTML directory, make sure you have watir submoduled'
+    logging.error(msg)
+    assert 0, msg
+
+DEFAULT_HOST = '127.0.0.1'
+DEFAULT_PORT = 8000
+
+
+
+class WebServer(object):
+    """A very basic web server."""
+    def __init__(self, host=DEFAULT_HOST, port=DEFAULT_PORT):
+        self.stop_serving = False
+        self.host = host
+        self.port = port
+        while True:
+            try:
+                self.server = HTTPServer((host, port), SimpleHTTPRequestHandler)
+                self.host = host
+                self.port = port
+                break
+            except socket.error:
+                logging.debug('port {} is in use, trying the next one'.format(port))
+                port += 1
+
+        self.thread = threading.Thread(target=self.run)
+
+    def run(self):
+        logging.debug('web server started')
+        while not self.stop_serving:
+            self.server.handle_request()
+        self.server.server_close()
+
+    def start(self):
+        chdir(HTML_ROOT)
+        self.thread.start()
+
+    def stop(self):
+        self.stop_serving = True
+        try:
+            # This is to force stop the server loop
+            urllib_request.URLopener().open('http://{}:{}'.format(self.host, self.port))
+        except IOError:
+            pass
+        logging.info('Shutting down the webserver')
+        self.thread.join()
+
+    def path_for(self, path):
+        return 'http://{}:{}/{}'.format(self.host, self.port, path)
