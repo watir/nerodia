646c6825acc6f9196d65b5c85db724a1266e84ec
Lucas Tierney <lucast1533@gmail.com>
2017-05-10 21:23:04 -0500

    complete collections tests

---
+++ tests/elements/collections_tests.py
@@ -0,0 +1,25 @@
+import pytest
+
+pytestmark = pytest.mark.page('collections.html')
+
+
+def test_returns_inner_elements_of_parent_element_having_different_html_tag(browser):
+    assert len(browser.span(id='a_span').divs()) == 2
+
+
+def test_returns_inner_elements_of_parent_element_having_same_html_tag(browser):
+    assert len(browser.span(id='a_span').spans()) == 2
+
+
+def test_returns_correct_subtype_of_elements(browser):
+    from watir_snake.elements.html_elements import Span
+    collection = browser.span(id='a_span').spans().to_list
+    assert all(isinstance(el, Span) for el in collection)
+
+
+@pytest.mark.page('nested_elements.html')
+def test_can_contain_more_than_one_type_of_element(browser):
+    from watir_snake.elements.html_elements import Div, Span
+    collection = browser.div(id='parent').children()
+    assert any(isinstance(el, Span) for el in collection)
+    assert any(isinstance(el, Div) for el in collection)
