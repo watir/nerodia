6f7fb4fbbefa1bedaafb16e2c8192f4cadf5b055
Lucas Tierney <lucast1533@gmail.com>
2017-05-31 07:17:35 -0500

    add dl tests

---
+++ tests/elements/dl_tests.py
@@ -0,0 +1,132 @@
+import pytest
+
+pytestmark = pytest.mark.page('definition_lists.html')
+
+
+class TestDlExist(object):
+    def test_returns_true_if_the_element_exists(self, browser):
+        assert browser.dl(id='experience-list').exists
+        assert browser.dl(class_name='list').exists
+        assert browser.dl(xpath="//dl[@id='experience-list']").exists
+        assert browser.dl(index=0).exists
+
+    def test_returns_the_first_div_if_given_no_args(self, browser):
+        assert browser.dl().exists
+
+    def test_returns_false_if_the_element_doesnt_exist(self, browser):
+        assert not browser.dl(id='no_such_id').exists
+
+    def test_raises_correct_exception_when_what_argument_is_invalid(self, browser):
+        with pytest.raises(TypeError):
+            browser.dl(id=3.14).exists
+
+    def test_raises_correct_exception_when_how_argument_is_invalid(self, browser):
+        from watir_snake.exception import MissingWayOfFindingObjectException
+        with pytest.raises(MissingWayOfFindingObjectException):
+            browser.dl(no_such_how='some_value').exists
+
+
+class TestDlAttributes(object):
+    # class_name
+
+    def test_returns_the_class_name_if_element_exists(self, browser):
+        assert browser.dl(id='experience-list').class_name == 'list'
+
+    def test_returns_an_empty_string_if_element_exists_but_class_name_doesnt(self, browser):
+        assert browser.dl(id='noop').class_name == ''
+
+    @pytest.mark.parametrize('selector',
+                             [{'id': 'no_such_id'},
+                              {'title': 'no_such_title'},
+                              {'index': 1337},
+                              {'xpath': "//dl[@id='no_such_id']"}])
+    def test_raises_correct_exception_for_class_name_if_element_does_not_exist(self, browser, selector):
+        from watir_snake.exception import UnknownObjectException
+        with pytest.raises(UnknownObjectException):
+            browser.div(**selector).class_name
+
+    # id
+
+    def test_returns_the_id_attribute_if_element_exists(self, browser):
+        assert browser.dl(class_name='list').id == 'experience-list'
+
+    def test_returns_an_empty_string_if_element_exists_but_id_doesnt(self, browser):
+        assert browser.dl(class_name='personalia').id == ''
+
+    @pytest.mark.parametrize('selector',
+                             [{'id': 'no_such_id'},
+                              {'title': 'no_such_title'},
+                              {'index': 1337}])
+    def test_raises_correct_exception_for_id_if_element_does_not_exist(self, browser, selector):
+        from watir_snake.exception import UnknownObjectException
+        with pytest.raises(UnknownObjectException):
+            browser.dl(**selector).id
+
+    # title
+
+    def test_returns_the_title_attribute_if_the_element_exists(self, browser):
+        assert browser.dl(class_name='list').title == 'experience'
+
+    # text
+
+    def test_returns_the_text_of_the_element(self, browser):
+        assert '11 years' in browser.dl(id='experience-list').text
+
+    def test_returns_an_empty_string_if_element_exists_but_contains_no_text(self, browser):
+        assert browser.dl(id='noop').text == ''
+
+    @pytest.mark.parametrize('selector',
+                             [{'id': 'no_such_id'},
+                              {'title': 'no_such_title'},
+                              {'index': 1337},
+                              {'xpath': "//dl[@id='no_such_id']"}])
+    def test_raises_correct_exception_for_class_name_if_element_does_not_exist(self, browser, selector):
+        from watir_snake.exception import UnknownObjectException
+        with pytest.raises(UnknownObjectException):
+            browser.delete(**selector).text
+
+
+def test_finds_all_attribute_methods(browser):
+    assert hasattr(browser.dl(index=0), 'id')
+    assert hasattr(browser.dl(index=0), 'class_name')
+    assert hasattr(browser.dl(index=0), 'style')
+    assert hasattr(browser.dl(index=0), 'text')
+    assert hasattr(browser.dl(index=0), 'title')
+
+
+class TestDlManipulation(object):
+    # click
+
+    def test_fires_events_when_clicked(self, browser):
+        assert browser.dt(id='name').text != 'changed!'
+        browser.dt(id='name').click()
+        assert browser.dt(id='name').text == 'changed!'
+
+    @pytest.mark.parametrize('selector',
+                             [{'id': 'no_such_id'},
+                              {'title': 'no_such_title'},
+                              {'index': 1337},
+                              {'xpath': "//dl[@id='no_such_id']"}])
+    def test_raises_correct_exception_when_clicking_a_dl_that_doesnt_exist(self, browser, selector):
+        from watir_snake.exception import UnknownObjectException
+        with pytest.raises(UnknownObjectException):
+            browser.button(**selector).click()
+
+    # html
+
+    # TODO xfail IE
+    def test_returns_the_html_of_the_element(self, browser):
+        html = browser.dl(id='experience-list').html.lower()
+        assert '<dt class="current-industry">' in html
+        assert '</body>' not in html
+
+    # to_dict
+
+    def test_converts_the_dl_to_a_dict(self, browser):
+        expected = {
+            'Experience': '11 years',
+            'Education': 'Master',
+            'Current industry': 'Architecture',
+            'Previous industry experience': 'Architecture'
+        }
+        assert browser.dl(id='experience-list').to_dict() == expected
--- watir_snake/elements/dlist.py
+++ watir_snake/elements/dlist.py
@@ -7,6 +7,6 @@ from ..meta_elements import MetaHTMLElement
 @six.add_metaclass(MetaHTMLElement)
 class DList(HTMLElement):
     def to_dict(self):
-        keys = [e.text for e in self.dts]
-        values = [e.text for e in self.dds]
+        keys = [e.text for e in self.dts()]
+        values = [e.text for e in self.dds()]
         return dict(zip(keys, values))
