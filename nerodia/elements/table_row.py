ea7ea4a1ae4aa4ad19156f2442a55dba952ef331
lmtierney <lucast1533@gmail.com>
2017-11-28 15:12:36 -0600

    update table_row.py

--- nerodia/elements/table_row.py
+++ nerodia/elements/table_row.py
@@ -7,10 +7,24 @@ from ..meta_elements import MetaHTMLElement
 
 @six.add_metaclass(MetaHTMLElement)
 class TableRow(CellContainer, HTMLElement):
+    def __iter__(self):
+        """
+        Yields each TableCell associated with this row
+        :rtype: iter
+
+        :Example:
+
+        row = browser.tr()
+        for cell in row:
+            print(cell.text)
+        """
+        for cell in self.cells():
+            yield cell
+
     def __getitem__(self, idx):
         """
         Get the nth cell (<th> or <td>) of this row
         :param idx: index to get
         :rtype: nerodia.elements.cell.Cell
         """
-        return self.cell('index', idx)
+        return self.cell(index=idx)
