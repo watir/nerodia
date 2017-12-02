1fbc50b7e0b07f65b2bbd4e2dab357996113af2e
lmtierney <lucast1533@gmail.com>
2017-12-02 13:43:52 -0600

    add label_tests and labels_tests

---
+++ tests/elements/labels_tests.py
@@ -0,0 +1,24 @@
+import pytest
+
+pytestmark = pytest.mark.page('forms_with_input_elements.html')
+
+
+class TestLabels(object):
+    def test_with_selectors_returns_the_matching_elements(self, browser):
+        assert browser.labels('for', 'new_user_first_name').to_list == \
+            [browser.label('for', 'new_user_first_name')]
+
+    def test_returns_the_correct_number_of_labels(self, browser):
+        assert len(browser.labels()) == 40
+
+    def test_get_item_returns_the_image_at_the_given_index(self, browser):
+        assert browser.labels()[0].id == 'first_label'
+
+    def test_iterates_through_labels_correctly(self, browser):
+        count = 0
+
+        for index, label in enumerate(browser.labels()):
+            assert label.id == browser.label(index=index).id
+            count += 1
+
+        assert count > 0
