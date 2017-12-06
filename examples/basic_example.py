dbf47027667567f06d2578f68a7159fabad91fc0
joshin4colours <joshua.m.grant@gmail.com>
2017-12-06 13:35:04 -0500

    added a full but basic example for reference/documentation (#4)
    * added a full but basic example for reference/documentation
    
    * updated example to be less-Titus centric

---
+++ examples/basic_example.py
@@ -0,0 +1,19 @@
+"""Basic script showing how Nerodia works."""
+from nerodia.browser import Browser
+
+br = Browser(browser='firefox')
+
+br.goto("https://watir.com")
+
+# Check that "Titus" is somewhere in the page text
+assert "Watir" in br.text
+
+# Check "open source" is in the intro
+intro_text = br.div(class_name='intro').text
+assert "open source" in intro_text
+
+# Check that the page is correct via the URL
+br.link(text='Guides').click()
+assert 'watir.com/guides/' in br.url
+
+br.close()
