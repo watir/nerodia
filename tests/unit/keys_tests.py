0b8d2d33b07580e09a8e6d180f8cdd39276a63f2
joshgrant <joshua.m.grant@gmail.com>
2019-11-14 19:51:29 -0500

    better fix for python 2 vs 3

--- tests/unit/keys_tests.py
+++ tests/unit/keys_tests.py
@@ -1,15 +1,9 @@
-import six
-
-
 from nerodia.keys import Keys
 
 
 def test_can_access_keys():
-    if six.PY3:
-        assert Keys.TAB == '\ue004'
-    else:
-        assert Keys.TAB == '\\\ue004'
-
+        assert Keys.TAB == u'\ue004'
+    
 
 def test_keys_exists():
     assert isinstance(Keys, object)
